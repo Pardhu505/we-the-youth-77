c4217ed10af343f49f9a4d84d9536b719339f9f2
Pardhu505 <168645593+Pardhu505@users.noreply.github.com>
2025-09-16 11:32:49 +0530

    Update VideoSection.tsx

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -7,7 +7,7 @@ export const VideoSection = () => {
         data-testid="video-frame-image"
         className="w-full h-auto"
       />
-     <div className="absolute w-[77.5%] h-[72%] top-[2.5%] left-[12.3%] bg-white flex items-center justify-center">
+     <div className="absolute w-[75%] h-[72%] top-[2.5%] left-[12.3%] bg-white flex items-center justify-center">
         <p className="text-black text-2xl font-bold">Video coming soon</p>
       </div>
     </div>
